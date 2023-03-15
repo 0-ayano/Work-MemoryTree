7e3ef8c4a5bb2dc4adf81a729d86643021ac3eb7
0-ayano <mi181350@edu.okinawa-ct.ac.jp>
2023-03-15 15:43:10 +0900

    最低限の実装の終了

--- back/api/python/main.py
+++ back/api/python/main.py
@@ -1,7 +1,9 @@
 import pymysql.cursors
+import base64
 from starlette.middleware.cors import CORSMiddleware # 追加
 from fastapi import FastAPI
 from pydantic import BaseModel
+import model.modelWordCloud as mwc
 
 app = FastAPI()
 
@@ -121,6 +123,41 @@ def getDiary(day, title):
             "main" : result[0]["main"],
         }
 
+@app.get("/tree")
+def createImage():
+    connection = pymysql.connect(host='localhost',
+                                user='root',
+                                password='root',
+                                database='web',
+                                cursorclass=pymysql.cursors.DictCursor)
+
+    with connection:
+        with connection.cursor() as cursor:
+            # データ読み込み
+            sql = "SELECT * FROM diary order by day"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            # print(result)
+    
+    boxMain = []
+    for box in result:
+        boxMain.append(box["main"])
+
+    src = ""
+    for box in boxMain:
+        src = src + box
+
+    mwc.create_wordcrowd(src)
+
+    with open("WC.png", "rb") as img_file:
+        b64_string = base64.b64encode(img_file.read())
+        b64_string = str(b64_string)
+        ret = "data:image/png;base64,{}".format(b64_string[2:-1])
+
+    return{
+        "msg" : "--DO--",
+        "img" : ret
+    }
 
 
 """
